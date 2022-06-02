ecf5d705dbce9fb43fde80308d5deabd10539aa2
Eric Fernandez <efernandezalbuq@gmail.com>
2022-06-02 02:02:47 -0300

    Menu superior

--- poc/poc2/backend/frontend/src/components/Menu.js
+++ poc/poc2/backend/frontend/src/components/Menu.js
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import React, { Component } from "react";
 
 export default class Menu extends Component {
@@ -7,7 +8,17 @@ export default class Menu extends Component {
 
     render() {
         return (
-            <h1>Menu aqui</h1>
+            <div className="menu">
+                <div></div>
+                <div className="center">
+                    <div><a className="menu-item" href="/">Home</a></div>
+                    <div><a className="menu-item" href='/nova'>Cursos</a></div>
+                    <div><a className="menu-item">Quem somos</a></div>
+                </div>
+                <div className="login">
+                    <Button variant='contained' color='primary'>Login</Button>
+                </div>
+            </div>
         );
     }
 }
